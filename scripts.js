257fa304aa764169b9673903d6633e3c2e745435
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-01-11 20:47:30 +0000

    created operate function and key variables
    added main operate function and defined key variables for the two
    numbers and the required function

--- scripts.js
+++ scripts.js
@@ -14,3 +14,25 @@ function divide (num1, num2) {
     return num1 / num2;
 };
 
+let firstNumber; 
+let secondNumber;
+let operator;
+
+function operate (num1, num2, operator) {
+    switch (operator) {
+        case "add":
+            return add(num1,num2);
+            break;
+        case "subtract":
+            return subtract(num1, num2);
+            break;
+        case "multiply":
+            return multiply(num1, num2);
+            break;
+        case "divide":
+            return divide(num1, num2);
+            break;
+    }
+};
+
+console.log(operate(3,2,"divide"));
